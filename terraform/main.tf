c156a420d20259fa06c6e120819aea5748522973
Luciano Lionello <lucianolio@protonmail.com>
2023-11-18 20:09:54 -0300

    Test Apprunner deployment

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+
+locals {
+  service_name = var.service_name
+  tags         = { "scope" = "eko" }
+  image        = "public.ecr.aws/aws-containers/hello-app-runner:latest"
+  port         = 8000
+
+}
+
+resource "aws_apprunner_auto_scaling_configuration_version" "autoscaling_configuration" {
+  auto_scaling_configuration_name = "${local.service_name}-autoscaling-configuration"
+
+  max_concurrency = 100
+  max_size        = 2
+  min_size        = 1
+
+  tags = local.tags
+
+}
+
+resource "aws_apprunner_service" "service" {
+
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.autoscaling_configuration.arn
+  service_name                   = local.service_name
+
+  health_check_configuration {
+    healthy_threshold   = 1
+    interval            = 10
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 5
+  }
+
+  instance_configuration {
+    cpu    = "256"
+    memory = "512"
+  }
+
+  source_configuration {
+
+    # Must be false when using public images or cross account images
+    auto_deployments_enabled = false
+
+    image_repository {
+      image_configuration {
+        port = local.port
+      }
+
+      image_identifier      = local.image
+      image_repository_type = "ECR Public"
+    }
+  }
+
+  tags = local.tags
+
+}
+
+
