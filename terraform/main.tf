fbc89ed4395c28e6ca82aa98d3528b5cf428ab17
Luciano Lionello <lucianolio@protonmail.com>
2023-11-20 09:50:41 -0300

    use vulnerable app image

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ data "aws_caller_identity" "current" {}
 locals {
   service_name = "luciano-lionello"
   tags         = { "scope" = "eko" }
-  image        = "public.ecr.aws/aws-containers/hello-app-runner:${var.image_tag}"
+  image        = "${aws_ecr_repository.application_registry.repository_url}:${var.image_tag}"
   port         = 8000
 
 }
